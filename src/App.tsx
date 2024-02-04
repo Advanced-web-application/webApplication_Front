cffd998aab60e7c23909ff2ddbf91e4e9738376f
Michal Massachi <michal2792000@gmail.com>
2024-02-04 18:38:31 +0200

    merged main

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
-import { useState } from "react"
-import ListGroup from "./ListGroup"
-import Alert from "./Alert"
+// import { useState } from "react"
+// import ListGroup from "./ListGroup"
+// import Alert from "./Alert"
 
-function App() {
-  const [cities, setCities] = useState(["London", "New York", "Paris", "Tokyo", "Delhi", "Dubai"])
-  const [cars, setCars] = useState(["BMW", "Mercedes", "Audi", "Lamborghini", "Ferrari", "Porsche"])
-  const [displayAleret, setDisplayAlert] = useState(false)
+// function App() {
+//   const [cities, setCities] = useState(["London", "New York", "Paris", "Tokyo", "Delhi", "Dubai"])
+//   const [cars, setCars] = useState(["BMW", "Mercedes", "Audi", "Lamborghini", "Ferrari", "Porsche"])
+//   const [displayAleret, setDisplayAlert] = useState(false)
 
-  const onDeleteCity = (index: number) => {
-    console.log("Deleting city at index: " + index)
-    const tmp = cities.filter((city, i) => i !== index)
-    setCities(tmp)
-  }
+//   const onDeleteCity = (index: number) => {
+//     console.log("Deleting city at index: " + index)
+//     const tmp = cities.filter((city, i) => i !== index)
+//     setCities(tmp)
+//   }
 
-  const onDeleteCar = (index: number) => {
-    console.log("Deleting car at index: " + index)
-    setCars(cars.filter((car, i) => i !== index))
-  }
+//   const onDeleteCar = (index: number) => {
+//     console.log("Deleting car at index: " + index)
+//     setCars(cars.filter((car, i) => i !== index))
+//   }
 
-  const openAlert = () => {
-    setDisplayAlert(true)
-  }
+//   const openAlert = () => {
+//     setDisplayAlert(true)
+//   }
 
-  const onDismiss = () => {
-    setDisplayAlert(false)
-  }
-  return (
-    <div className="p-2">
-      {displayAleret && <Alert onDismiss={onDismiss}>This is an Alert!!!</Alert>}
-      <h1>Cities!!</h1>
-      <ListGroup items={cities} onDeleteItem={onDeleteCity} />
+//   const onDismiss = () => {
+//     setDisplayAlert(false)
+//   }
+//   return (
+//     <div className="p-2">
+//       {displayAleret && <Alert onDismiss={onDismiss}>This is an Alert!!!</Alert>}
+//       <h1>Cities!!</h1>
+//       <ListGroup items={cities} onDeleteItem={onDeleteCity} />
 
-      <h1>Cars!!</h1>
-      <ListGroup items={cars} onDeleteItem={onDeleteCar} />
-      <div className="mx-auto pt-2 d-flex" style={{ width: "300px" }}>
-        <button type="button"
-          className="btn btn-primary flex-fill"
-          onClick={openAlert}>
-          Open alert
-        </button>
-      </div>
-    </div>
-  )
-}
+//       <h1>Cars!!</h1>
+//       <ListGroup items={cars} onDeleteItem={onDeleteCar} />
+//       <div className="mx-auto pt-2 d-flex" style={{ width: "300px" }}>
+//         <button type="button"
+//           className="btn btn-primary flex-fill"
+//           onClick={openAlert}>
+//           Open alert
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
 
 // import PostsList from "./PostsList"
 
@@ -54,6 +54,17 @@ function App() {
 //     </div>
 //   )
 // }
+
+import StudentForm from "./StudentForm"
+
+function App() {
+
+  return (
+    <div className="p-2">
+      <StudentForm />
+    </div>
+  )
+}
 
 
 export default App
