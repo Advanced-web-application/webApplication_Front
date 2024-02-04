32081e3f22b058ede4657dfa6c4e5107ecd3624f
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-02-04 17:34:45 +0200

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -44,24 +44,15 @@
 //   )
 // }
 
-
-import React from "react";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
 import Registration from "./components/Registration"
-
 
 function App() {
   return (
-    <Router>
-
-        <Route path="/register">
-          <div className="p-2">
-            <Registration />
-          </div>
-        </Route>
-       
-    </Router>
+    <div className="p-2">
+      <Registration />
+    </div>
   )
 }
+
 
 export default App
