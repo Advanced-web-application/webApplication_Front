5d64de3e91367240eaaca99ce8eb8acdec9108bd
Michal Massachi <michal2792000@gmail.com>
2024-02-05 15:15:18 +0200

    startimg the editProfile

---
+++ src/components/ProfileEdit.tsx
@@ -0,0 +1,19 @@
+// import { useEffect, useState } from 'react'
+
+
+// const UpadateProfile = () => {
+
+//     const [name, aetName] = useState("")
+//     const [age, setAge] = useState("")
+//     const [gender, setGender] = useState("")
+//     const [email, setEmail] = useState("")
+//     const [password, setPassword] = useState("")
+//     const [img, setImg] = useState("")
+//     const [error, setError] = useState("")
+
+//     return ()
+//         <div>
+//             <h1>Update Profile</h1>
+//         </div>
+//     )
+// }
