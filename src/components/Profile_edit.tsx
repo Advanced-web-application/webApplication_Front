c7201437afa1e5aebdb96af7a24e42ffc198dd93
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-03-13 19:07:17 +0200

    successful image uploading

--- src/components/Profile_edit.tsx
+++ src/components/Profile_edit.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState,useRef } from 'react';
 import profileService, { CanceledError } from '../services/profile-service';
 import { IUser } from '../ProfileDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +6,10 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import z from "zod";
+import avatar from '../assets/avatar.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
+import { uploadPhoto } from '../services/file-service'
 
 const schema = z.object({
     fullName: z.string().min(3, "Full Name must be longer than 3 characters"),
@@ -18,7 +22,7 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
-function EditProfile() {
+function ProfileEdit() {
     const navigate = useNavigate();
     const location = useLocation();
     const userID = location.state?.userID;
@@ -26,7 +30,7 @@ function EditProfile() {
     const { register, handleSubmit, formState: { errors }, setValue } = useForm<FormData>({ resolver: zodResolver(schema) });
 
     const [user, setUser] = useState<IUser>();
-    const [age, setAge] = useState<number | undefined>();
+    const [imgSrc, setImgSrc] = useState<File>();
 
     useEffect(() => {
         const abortController = new AbortController();
@@ -40,13 +44,11 @@ function EditProfile() {
 
                 if (user) {
                     setUser(user);
-                    // Set default values for the form fields
                     setValue("fullName", user.fullName);
                     setValue("age", user.age);
                     setValue("gender", user.gender);
                     setValue("email", user.email);
                     setValue("image", user.image);
-                    setAge(user.age); // Set age for the age field
                 }
             } catch (err) {
                 console.log(err);
@@ -62,12 +64,19 @@ function EditProfile() {
     }, [userID, setValue]);
 
     const onSubmit = async (data: FormData) => {
+        let imageUrl;
+        if (imgSrc) {
+            imageUrl = await uploadPhoto(imgSrc);
+        } else {
+            imageUrl = data.image;
+        }
+
         const updatedProfile = {
             fullName: data.fullName,
             age: data.age,
             gender: data.gender,
             _id: data._id,
-            image: data.image,
+            image: imageUrl,
             email: data.email
         };
 
@@ -76,13 +85,32 @@ function EditProfile() {
         navigate('/profile', { state: { userID } });
     };
 
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const imgSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            setImgSrc(e.target.files[0]);
+        }
+    }
+
+    const onImageUploadButtonClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    }
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="m-3">
             <h1>Edit Profile</h1>
             <div className="mb-3">
                 {user && user.image && <img src={user.image} alt="User" className="img-thumbnail mb-2" style={{ maxWidth: '200px' }} />}
-                <label className="form-label">Image:</label>
-                <input type="text" className="form-control" {...register("image")} defaultValue={user?.image} />
+                <div className="d-flex justify-content-center position-relative">
+                    {imgSrc && <img src={URL.createObjectURL(imgSrc)} style={{ height: "230px", width: "230px" }} className="img-fluid" />}
+                    <button type="button" className="btn position-absolute bottom-0 end-0" onClick={onImageUploadButtonClick}>
+                        <FontAwesomeIcon icon={faImage} className="fa-xl" />
+                    </button>
+                </div>
+                <input style={{ display: "none" }} {...register("image")} type="file" onChange={imgSelected} ref={fileInputRef}></input>
                 {errors.image && <p className="text-danger">{errors.image.message}</p>}
             </div>
 
@@ -94,7 +122,7 @@ function EditProfile() {
 
             <div className="mb-3">
                 <label className="form-label">Age:</label>
-                <input type="number" min="18" className="form-control" {...register("age", { setValueAs: value => parseFloat(value) })} defaultValue={age} />
+                <input type="number" min="18" className="form-control" {...register("age")} defaultValue={user?.age} />
                 {errors.age && <p className="text-danger">{errors.age.message}</p>}
             </div>
 
@@ -115,4 +143,4 @@ function EditProfile() {
     )
 }
 
-export default EditProfile;
+export default ProfileEdit;
